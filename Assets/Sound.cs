7447de17c157bbd0413de41ab58a72935bf09b28
dewipitri <dewipitri02@gmail.com>
2026-09-11 16:33:01 +0700

    add: new sprites

--- Assets/Sound.cs
+++ Assets/Sound.cs
@@ -5,6 +5,7 @@ public class Sound : MonoBehaviour
 {
     [SerializeField] Slider volumeSlider;
 
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         if (!PlayerPrefs.HasKey("musicVolume"))
