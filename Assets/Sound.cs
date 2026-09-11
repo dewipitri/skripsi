210059caed18ce1d50e70a2a0260c822eb31d846
dewipitri <dewipitri02@gmail.com>
2026-09-11 12:58:57 +0700

    fix:comment

--- Assets/Sound.cs
+++ Assets/Sound.cs
@@ -5,7 +5,6 @@ public class Sound : MonoBehaviour
 {
     [SerializeField] Slider volumeSlider;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         if (!PlayerPrefs.HasKey("musicVolume"))
